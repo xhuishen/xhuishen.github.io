cc18230d533249ccf42020bb160dd927eaf6a6b2
shenxiaohui <978007533@qq.com>
2024-01-05 10:25:54 +0800

    '11'

---
+++ static/assets/js/icon-x12wglUL.js
@@ -0,0 +1,16 @@
+import{_,j as h,o as r,f as p,h as s,a as t,w as i,t as l,l as u,u as d,p as v,g as f,C as a}from"./index-T7UBcAV8.js";const o=e=>(v("data-v-3f1845d0"),e=e(),f(),e),m={class:"iconPage"},x=o(()=>s("div",{class:"head"},[s("div",{class:"title"},[l(" 图标 "),s("span",null,"ICON")])],-1)),g={class:"body"},B={class:"card"},w={class:"cardHead"},C={class:"title"},I=o(()=>s("span",null,"单色 且 Element Plus支持",-1)),y={class:"cardBody"},E={class:"readme"},z={class:"card"},N={class:"cardHead"},j={class:"title"},k=o(()=>s("span",null,"多色 或 Element Plus不支持",-1)),L={class:"cardBody"},P={class:"readme"},S=o(()=>s("p",null,"1.登录iconfont官网 https://www.iconfont.cn/ 选择资源管理>我的项目降图标下载到本地且导入项目",-1)),V=o(()=>s("p",null,"2.在项目中引入iconfont.css 需要兼容旧版本浏览器的使用unicode方式",-1)),b=o(()=>s("p",null,"3.定义iconfont公共样式",-1)),D=o(()=>s("p",null,".iconfont { ... }",-1)),H=o(()=>s("p",null,"4.使用",-1)),O=`<template>
+  <div>
+    <el-icon :size="size" :color="color">
+      <List />
+    </el-icon>
+    <!-- 或者独立使用它，不从父级获取属性 -->
+    <List />
+  </div>
+</template>
+// script
+import { List } from '@element-plus/icons-vue'`,T=`
+<i class="iconfont icon-shouye"></i> //font class
+<i class="iconfont">&#xe602;</i> //Unicode
+<svg class="icon" aria-hidden="true"> //symbol
+    <use xlink:href="#icon-xxx"></use>
+</svg>`,U={__name:"icon",setup(e){return(q,A)=>{const c=u,n=h("highlightjs");return r(),p("div",m,[x,s("div",g,[s("div",B,[s("div",w,[s("div",C,[t(c,{color:"#3D4B6C",size:"20"},{default:i(()=>[t(d(a))]),_:1}),I])]),s("div",y,[s("div",E,[l(" 如果是Element的单色图标 直接复制即可 "),t(n,{autodetect:"",code:O})])])]),s("div",z,[s("div",N,[s("div",j,[t(c,{color:"#3D4B6C",size:"20"},{default:i(()=>[t(d(a))]),_:1}),k])]),s("div",L,[s("div",P,[S,V,b,D,H,t(n,{autodetect:"",code:T})])])])])])}}},G=_(U,[["__scopeId","data-v-3f1845d0"]]);export{G as default};
