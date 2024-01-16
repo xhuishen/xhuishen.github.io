771ea765f5883646399acd1b4c30a533e6c6d802
shenxiaohui <978007533@qq.com>
2024-01-16 16:22:44 +0800

    'UI更新'

---
+++ static/assets/js/other-qjIlqGmR.js
@@ -0,0 +1,36 @@
+import{_ as n,v as i,o as d,c as _,a as s,b as t,w as l,E as r,u as p,p as u,g as h,s as v}from"./index-5Ty6p_7s.js";const o=e=>(u("data-v-8568f8f1"),e=e(),h(),e),m={class:"otherPage"},g={class:"body"},f={class:"card"},j={class:"cardHead"},E={class:"title"},x=o(()=>s("span",null,"其他",-1)),k={class:"cardBody"},y={class:"readme"},A=o(()=>s("p",null,"本项目目录结构",-1)),B=`
+├─.eslintrc.cjs -------------------------- eslint配置
+├─.gitignore ----------------------------- git忽略文件
+├─.prettierrc.json ----------------------- prettier配置
+├─index.html ----------------------------- 入口html
+├─jsconfig.json -------------------------- jsconfig配置
+├─package-lock.json
+├─package.json --------------------------- 项目依赖
+├─README.md ------------------------------ README.md
+├─vite.config.js ------------------------- vite构建配置
+├─src
+| ├─App.vue ------------------------------ 根页面
+| ├─main.js ------------------------------ 入口js
+| ├─views -------------------------------- 页面目录
+| ├─utils -------------------------------- 工具类
+| ├─plugins ------------------------------ 插件如请求封装Axios
+| ├─stores ------------------------------- 状态管理
+| ├─router ------------------------------- 路由配置
+| ├─layouts ------------------------------ 布局目录
+| | ├─DefaultPage.vue --------------- 默认布局
+| | └EmptyPage.vue ------------------ 空白布局
+| ├─components ---------------------------- 全局组件
+| | ├─Aside.vue --------------------- 侧边栏
+| | ├─Footer.vue -------------------- 底部
+| | ├─Header.vue -------------------- 头部
+| | └RightMenu.vue ------------------ 右侧菜单
+| ├─assets -------------------------------- 静态资源
+| | ├─styles ------------------------ 全局样式
+| | | ├─_element.default.scss ---- element scss变量备份文件(未引用，主要为了查找变量名称)
+| | | ├─_reset.scss ------------- 全局样式重置
+| | | └_var.scss ---------------- 全局scss变量
+| | ├─images ------------------------ 图片
+| | ├─font -------------------------- 字体
+├─public
+| └favicon.ico
+`,I={__name:"other",setup(e){return(w,D)=>{const c=r,a=i("highlightjs");return d(),_("div",m,[s("div",g,[s("div",f,[s("div",j,[s("div",E,[t(c,{color:"#3D4B6C",size:"20"},{default:l(()=>[t(p(v))]),_:1}),x])]),s("div",k,[s("div",y,[A,t(a,{code:B})])])])])])}}},C=n(I,[["__scopeId","data-v-8568f8f1"]]);export{C as default};
